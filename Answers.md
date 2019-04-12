2ae04878094457ef995dcb90cf38452ad0f92e5c
Tristan Grovender <therealtristangrovender@gmail.com>
2019-04-12 12:22:03 -0500

    MVP complete

--- Answers.md
+++ Answers.md
@@ -5,18 +5,19 @@
 
 1.  Describe `actions`, `reducers` and the `store` and their role in Redux. What does each piece do? Why is the store known as a 'single source of truth' in a redux application?
 
--Actions are to a reducer then the reducer does something to the state, the store is where all state is held.
+-Actions go to a reducer then the reducer does something to the state, the store is where all state is held.
 
 1.  What is the difference between Application state and Component state? When would be a good time to use one over the other?
 
+- Application state can be accessed by all components where as component state cannot. If you only need to use state within 1 component then component state is a good choice, app state is a good choice if multiple components might need that state.
 
 1.  What is middleware?
 
-Middleware takes and action and manipulates it before sending it on to the reducer.
+-Middleware takes an action and manipulates it before sending it on to the reducer.
 
 1.  Describe `redux-thunk`, what does it allow us to do? How does it change our `action-creators`?
 
-This allows us to run asynchronous JS.
+-Redux Thunk middleware allows you to write action creators that return a function instead of an action. The thunk can be used to delay the dispatch of an action, or to dispatch only if a certain condition is met. The inner function receives the store methods dispatch and getState as parameters.
 
 1.  Which `react-redux` method links up our `components` with our `redux store`?
 
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -21,3 +21,104 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+import {
+  GET_SMURFS,
+  GET_SMURFS_FAILURE,
+  GET_SMURFS_SUCCESS,
+  ADD_SMURF,
+  ADD_SMURF_FAILURE,
+  ADD_SMURF_SUCCESS,
+  UPDATE_SMURF,
+  UPDATE_SMURF_FAILURE,
+  UPDATE_SMURF_SUCCESS,
+  DELETE_SMURF,
+  DELETE_SMURF_SUCCESS,
+  DELETE_SMURF_FAILURE
+} from '../actions';
+
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+};
+
+const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_SMURFS:
+      return {
+        ...state,
+        fetchingSmurfs: true
+      };
+    case GET_SMURFS_SUCCESS:
+      return {
+        ...state,
+        smurfs: [...action.payload],
+        fetchingSmurfs: false
+      };
+    case GET_SMURFS_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        fetchingSmurfs: false
+      };
+    case ADD_SMURF:
+      return {
+        ...state,
+        addingSmurf: true
+      };
+    case ADD_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: [...action.payload],
+        addingSmurf: false
+      };
+    case ADD_SMURF_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        addingSmurf: false
+      };
+    case UPDATE_SMURF:
+      return {
+        ...state,
+        updatingSmurf: true
+      };
+    case UPDATE_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: [...action.payload],
+        updatingSmurf: false
+      };
+    case UPDATE_SMURF_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        updatingSmurf: false
+      };
+    case DELETE_SMURF:
+      return {
+        ...state,
+        deletingSmurf: true
+      };
+    case DELETE_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: [...action.payload],
+        deletingSmurf: false
+      };
+    case DELETE_SMURF_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        deletingSmurf: false
+      };
+    default:
+      return state;
+  }
+};
+
+export default smurfReducer;
